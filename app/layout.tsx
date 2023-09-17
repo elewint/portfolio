dde38d80d2b70e41b0a94bb5fc6eb678229b67ed
elewint <elewint@gmail.com>
2023-09-16 18:28:26 -0700

    Initial commit with Next.js

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Eli Intriligator | Software Engineer',
+  description: 'Recent graduate with a passion for finding inventive technical solutions, drawing on a strong foundation in software engineering principles and experience with full stack development, visualization, and virtual reality. Actively seeking a career in software engineering.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-slate-50 text-slate-950 relative`}>
+        <div className='bg-amber-100 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
+        <div className='bg-blue-100 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        {children}
+      </body>
+    </html>
+  )
+}
